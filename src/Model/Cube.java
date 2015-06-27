c638b9698eba60ec73cf3073f766c4e7baec2e33
yura <yudzhadobin@gmail.com>
2015-06-27 04:04:33 +0300

    Model implemented

---
+++ src/Model/Cube.java
@@ -0,0 +1,42 @@
+package Model;
+
+import java.awt.*;
+
+public class Cube {
+    private final static Cube instance= new Cube();
+
+    final Side left = new Side(Color.YELLOW);
+    final Side top = new Side(Color.WHITE);
+    final Side right = new Side(Color.GREEN);
+    final Side front = new Side(Color.PINK);
+    final Side back = new Side(Color.ORANGE);
+    final Side bottom = new Side(Color.BLUE);
+
+    public Side getLeft() {
+        return left;
+    }
+
+    public Side getTop() {
+        return top;
+    }
+
+    public Side getRight() {
+        return right;
+    }
+
+    public Side getFront() {
+        return front;
+    }
+
+    public Side getBack() {
+        return back;
+    }
+
+    public Side getBottom() {
+        return bottom;
+    }
+
+    public static Cube getInstance() {
+        return instance;
+    }
+}
