602a7d1c75f7f624ae0a19bb39898ee9652529df
yura <yudzhadobin@gmail.com>
2015-06-28 03:04:36 +0300

    bug with image need to resize i dont know why... but we dont need it at all so i dont wanna fix it

--- src/View/MainForm.java
+++ src/View/MainForm.java
@@ -1,26 +1,40 @@
 package View;
 
 import Model.Cube;
-import com.sun.xml.internal.bind.v2.TODO;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
-/**
- * Created by Юрий on 27.06.2015.
- */
 public class MainForm  extends JFrame{
     public MainForm() {
         super();
-        this.setSize(new Dimension(1900,1900));
-       this.setVisible(true);
-       this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setSize(new Dimension(500, 500));
+        this.setVisible(true);
+        this.add(new ImagePanel());
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
-       MainForm form = new MainForm();
-       form.getGraphics().drawImage(Cube.getInstance().getView(),30,30,900,900,null);
+        MainForm form = new MainForm();
     }
 }
 
 
+class ImagePanel extends JPanel{
+
+    private Image image;
+
+    public ImagePanel() {
+        image = Cube.getInstance().getView();
+        this.setVisible(true);
+        this.repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, null); // see javadoc for more info on the parameters
+    }
+
+}
